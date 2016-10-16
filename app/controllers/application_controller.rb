268bd424c7efaaef2fe6746355205815f9d55687
gdshailes <greg.shailes@gmail.com>
2016-10-16 10:07:00 +0000

    Added Twitter API integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,34 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    # Display 'enter username' form.
+
+    # View will display 'enter username' form; which needs a blank twitter user
     @twitter_user = TwitterUser.new
 
-    #render html: "Enter Twitter Username"
   end
 
   def get_followers
 
     @TwitterUserName = params[:twitter_user][:UserName]
 
+    # Check we've got a username
     if @TwitterUserName.blank?
       render html: "Username not supplied"
       return
     end
 
-    render html: @TwitterUserName
+    # Stip the '@' off, if it's been entered - we don't need it
+    if @TwitterUserName.
+
+    # Search for followers of this user
+    @Followers = $twitter.followers("greg_shailes")
+
+    # Quick and dirty HTML render, until we create a view for the results.
+    @ToRender = ""
+    @Followers.each do |follower|
+      @ToRender = @ToRender + follower.name + "<br>"
+    end
+    render html: @ToRender
 
   end
 
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+
+$twitter = Twitter::REST::Client.new do |config|
+ config.consumer_key = '7xWh3fxbdLu2b5wPF2nA6fZsU'
+ config.consumer_secret = '94lS2LAhei0ljRTAP0QVTHm8PqG6jy6oCX9JE2ROZZi7hyCZmn'
+ config.access_token = '713035571610959873-5Ipq9NDcFo8s3C3cekolMZyGeeTwqlP'
+ config.access_token_secret = 'w3wk6XZF3DZ5R7RRbVovaHLHR6Leb8Widydypud75kCzA'
+end
