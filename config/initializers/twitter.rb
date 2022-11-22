4560d0a8bfe383d4fdf0ca3f34d1bfbde66b08b1
Greg Shailes <greg.shailes@gmail.com>
2022-11-22 16:03:31 +0000

    Update twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,7 @@
 
 $twitter = Twitter::REST::Client.new do |config|
- config.consumer_key = '7xWh3fxbdLu2b5wPF2nA6fZsU'
- config.consumer_secret = '94lS2LAhei0ljRTAP0QVTHm8PqG6jy6oCX9JE2ROZZi7hyCZmn'
- config.access_token = '713035571610959873-5Ipq9NDcFo8s3C3cekolMZyGeeTwqlP'
- config.access_token_secret = 'w3wk6XZF3DZ5R7RRbVovaHLHR6Leb8Widydypud75kCzA'
+ config.consumer_key = ENV['CONSUMER_KEY']
+ config.consumer_secret = ENV['CONSUMER_SECRET']
+ config.access_token = ENV['ACCESS_TOKEN']
+ config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
